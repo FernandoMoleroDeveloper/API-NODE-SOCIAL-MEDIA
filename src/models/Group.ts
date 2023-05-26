188bca20c3dd2ad5917a055ef229b6809ce3d654
diegomompo7 <diego.mompo.redoli@gmail.com>
2023-05-26 19:59:32 +0200

    api

--- src/models/Group.ts
+++ src/models/Group.ts
@@ -6,12 +6,12 @@ const Schema = mongoose.Schema;
 
 // Interface de Book
 export interface IGroup {
-    admin : ObjectId,
-    members: Array<ObjectId>,
-    name: string,
-    createDate: Date,
-    publication: Array<ObjectId>,
-    modificationDate: Date
+  admin: ObjectId;
+  members: ObjectId[];
+  name: string;
+  createDate: Date;
+  publication: ObjectId[];
+  modificationDate: Date;
 }
 
 // Creamos esquema del book:
@@ -19,10 +19,10 @@ const bookSchema = new Schema<IGroup>(
   {
     admin: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: false },
     members: { type: [mongoose.Schema.Types.ObjectId], ref: "User", required: false },
-    name: {type: String, trim: true, minLength: [3, " Al menos tres letras para el título"], maxLength: [40, "Título demasiado largo, máximo de 20 caracteres"], required: true },
+    name: { type: String, trim: true, minLength: [3, " Al menos tres letras para el título"], maxLength: [40, "Título demasiado largo, máximo de 20 caracteres"], required: true },
     createDate: { type: Date, required: true },
     publication: { type: [mongoose.Schema.Types.ObjectId], ref: "User", required: false },
-    modificationDate: { type: Date, required: false }
+    modificationDate: { type: Date, required: false },
   },
   { timestamps: true } // Cada vez que se modifique un documento refleja la hora y fecha de modificación
 );
