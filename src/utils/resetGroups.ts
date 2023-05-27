6fb4b3d7ca6e8511168defdb7bf6559f8f34ed0d
diegomompo7 <diego.mompo.redoli@gmail.com>
2023-05-27 13:00:52 +0200

    API-RED_SOCIAL

--- src/utils/resetGroups.ts
+++ src/utils/resetGroups.ts
@@ -1,7 +1,7 @@
 import { Group } from "../models/Group";
 import { Publication } from "../models/Publication";
 import { User } from "../models/User";
-// import { generateRandom } from "./generateRandom";
+import { generateRandom } from "./generateRandom";
 
 const groupList = [
   { name: "Grupo de Fotografía Urbana", createDate: new Date("2023-04-12T09:30:00") },
@@ -10,8 +10,8 @@ const groupList = [
 //  Función de reseteo de documentos de la colección.
 export const resetGroups = async (): Promise<void> => {
   try {
-    // const members = []
-    // const publication = []
+    const members = []
+    const publication = []
     await Group.collection.drop(); //  Esperamos a que borre los documentos de la colección group de la BBDD.
     console.log("Borrados grupos");
 
@@ -30,7 +30,7 @@ export const resetGroups = async (): Promise<void> => {
     // Para cada libro recogido elegimos un autor y una editorial al azar entre los existentes y se lo asignamos como una propiedad a cada libro.
     for (let i = 0; i < documents.length; i++) {
       const document = documents[i];
-      /* const randomUser = users[generateRandom(0, users.length)];
+      const randomUser = users[generateRandom(0, users.length)];
 
       members.push(
         users[generateRandom(0, users.length)],
@@ -43,8 +43,8 @@ export const resetGroups = async (): Promise<void> => {
         publications[generateRandom(0, publications.length)])
 
       document.admin = randomUser as unknown as any;
-      //document.members = members;
-      document.publication = publication; */
+      document.members = members as unknown as any
+      document.publication = publication as unknown as any;
       const group = document;
       await group.save();
     }
